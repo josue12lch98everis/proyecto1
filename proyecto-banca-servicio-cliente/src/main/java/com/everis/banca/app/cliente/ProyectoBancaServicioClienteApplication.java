a935cfae4c0e7848ca3fbf61a481827a3afb8098
mhuaccha <mhuaccha@LIM-2TGHTT2.mshome.net>
2021-08-03 17:28:24 -0500

    Inicio del microservicio cliente

---
+++ proyecto-banca-servicio-cliente/src/main/java/com/everis/banca/app/cliente/ProyectoBancaServicioClienteApplication.java
@@ -0,0 +1,13 @@
+package com.everis.banca.app.cliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoBancaServicioClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoBancaServicioClienteApplication.class, args);
+	}
+
+}
---
+++ proyecto-banca-servicio-cliente/src/test/java/com/everis/banca/app/cliente/ProyectoBancaServicioClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.everis.banca.app.cliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoBancaServicioClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
