2893bf8a8d2dd512248239b2b6b0e7582f84ed1b
Mauricio06Everis <mauricioandre.huacchapacheco@everis.nttdata.com>
2021-08-09 01:56:07 -0500

    comentando código de los ms plazo fijo, cuenta ahorro

--- proyecto-banca-servicio-cuenta-ahorro/banca.app.cuenta-ahorro/src/main/java/com/everis/banca/app/credito/services/interfaces/ICreditCardService.java
+++ proyecto-banca-servicio-cuenta-ahorro/banca.app.cuenta-ahorro/src/main/java/com/everis/banca/app/credito/services/interfaces/ICreditCardService.java
@@ -1,13 +1,13 @@
 package com.everis.banca.app.credito.services.interfaces;
 
-import com.everis.banca.app.credito.documents.CreditCard;
+// import com.everis.banca.app.credito.documents.CreditCard;
 
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public interface ICreditCardService {
 
-	Flux<CreditCard> getAllCreditCard();
+	// Flux<CreditCard> getAllCreditCard();
 	
-	Mono<CreditCard> createCreditCard(CreditCard creditCard);
+	// Mono<CreditCard> createCreditCard(CreditCard creditCard);
 }
